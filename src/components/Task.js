97f242dd289ecfd0dd8a810ab4cb72687a53872e
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-22 07:42:43 +0530

    URL changes

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,17 @@
 import { FaTimes } from 'react-icons/fa'
 
 const Task = ({ task, onDelete, onToggle }) => {
+  console.log(task);
   return (
     <div
       className={`task ${task.reminder && 'reminder'}`}
-      onDoubleClick={() => onToggle(task.id)}
+      onDoubleClick={() => onToggle(task._id)}
     >
       <h3>
         {task.text}{' '}
         <FaTimes
           style={{ color: 'red', cursor: 'pointer' }}
-          onClick={() => onDelete(task.id)}
+          onClick={() => onDelete(task._id)}
         />
       </h3>
       <p>{task.day}</p>
